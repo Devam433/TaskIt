0c627f644cea2cb2782b6a1c5ae273015404e7c3
Devam <devam433@gmail.com>
2025-05-13 22:29:57 +0530

    Update for deploy in render

--- Backend/server.js
+++ Backend/server.js
@@ -14,10 +14,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../Frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, '../Frontend/build/index.html'));
 });
 
 //Protecting routes globally. It will ensure that all the sub-routes will too get protected.
